1e74af6bb49b5b4ddd52b72b2a5d76435851dd10
Munnass95 <mehedihasan162120@gmail.com>
2021-11-04 21:45:35 +0600

    modal added

--- src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,10 +1,59 @@
+import { Container, Grid, Typography } from "@mui/material";
 import React from "react";
+import Booking from "../Booking/Booking";
 
 const AvailableAppointment = ({ date }) => {
+  const bookings = [
+    {
+      id: 1,
+      name: "Teeth Orthodontics",
+      time: "8:00 AM - 9.00 AM",
+      space: 10,
+    },
+    {
+      id: 2,
+      name: "Cosmetic Dentistry",
+      time: "10:05 AM - 11.30 AM",
+      space: 10,
+    },
+    {
+      id: 3,
+      name: "Teeth Cleaning",
+      time: "5:00 PM - 6.30 PM",
+      space: 10,
+    },
+    {
+      id: 4,
+      name: "Cavity Protection",
+      time: "7:00 AM - 8.30 AM",
+      space: 5,
+    },
+    {
+      id: 5,
+      name: "Padiatric Dental =",
+      time: "6:00 PM - 7.00 PM",
+      space: 10,
+    },
+    {
+      id: 6,
+      name: "Oral Surgery",
+      time: "7:00 PM - 8.00 PM",
+      space: 10,
+    },
+  ];
+
   return (
-    <div>
-      <h2>Available Appointment {date.toDateString()} </h2>
-    </div>
+    <Container>
+      <Typography variant="h4" sx={{ color: "info.main", mb: 3 }}>
+        Available Appointment on {date.toDateString()}{" "}
+      </Typography>
+
+      <Grid container spacing={2}>
+        {bookings.map((booking) => (
+          <Booking key={booking.id} booking={booking} date={date}></Booking>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
---
+++ src/Pages/Appointment/Booking/Booking.js
@@ -0,0 +1,43 @@
+import { Button, Grid, Paper, Typography } from "@mui/material";
+import React from "react";
+import BookingModal from "../BookingModal/BookingModal";
+
+const Booking = ({ booking, date }) => {
+  const [openBooking, setOpenBooking] = React.useState(false);
+  const handleBookingOpen = () => setOpenBooking(true);
+  const handleBookingClose = () => setOpenBooking(false);
+  const { name, time, space } = booking;
+  return (
+    <>
+      <Grid item xs={12} sm={6} md={4}>
+        <Paper sx={{ py: 5 }} elevation={3}>
+          <Typography
+            sx={{ color: "info.main", fontWeight: 700 }}
+            variant="h5"
+            gutterBottom
+            component="div"
+          >
+            {name}
+          </Typography>
+          <Typography variant="h6" gutterBottom component="div">
+            {time}
+          </Typography>
+          <Typography variant="caption" gutterBottom component="div">
+            {space} SPACES AVAILABLE
+          </Typography>
+          <Button onClick={handleBookingOpen} variant="contained">
+            BOOK APPOINTMENT
+          </Button>
+        </Paper>
+      </Grid>
+      <BookingModal
+        booking={booking}
+        openBooking={openBooking}
+        handleBookingClose={handleBookingClose}
+        date={date}
+      ></BookingModal>
+    </>
+  );
+};
+
+export default Booking;
